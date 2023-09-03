f5dd5092f59ce101a8487cfeb656c5eb9459bc58
Kyaa-A <asnaripacalna@gmail.com>
2023-09-03 16:44:51 +0800

    init

--- PalindromeNum.java
+++ PalindromeNum.java
@@ -1,6 +1,24 @@
 public class PalindromeNum {
     public static void main(String[] args) {
         
+        boolean sout = isPalindrome(121);
+        System.out.println(sout);
+    }
+    
+    public static boolean isPalindrome(int x) {
+
+        StringBuilder str = new StringBuilder();
+
+        String strConverted = String.valueOf(x);
+        str.append(strConverted);
+
+        String word1 = str.toString();
+        String word2 = str.reverse().toString();
+
+        if(word1.equals(word2)){
+            return true;
+        }
+        return false;
         
     }
 }
